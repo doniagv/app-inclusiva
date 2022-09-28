2e0e90de24abae5456c7b204099d35d4ebe5fda3
doniagv <doniagv@gmail.com>
2022-09-27 20:11:08 -0500

    Se agregó el componente para login con google

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,39 @@
-export default function EnterPage({}) {
+import { auth, googleAuthProvider } from "../lib/firebase";
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
   return (
     <main>
-      <p className="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white">Sign Up</p>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        {user ? !username ? <UsernameForm /> : <SignOutButton /> : <SignInButton />}
+      </div>
     </main>
   );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
